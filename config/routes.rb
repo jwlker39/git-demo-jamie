593d1b8750379dca12f27a05f817b1b833e59ae2
Jamie Walker <jwlker39@memphis.edu>
2018-09-18 03:59:12 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'home', to: 'pages#home', as: 'home'
-  get 'about', to: 'pages#about', as: 'about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
